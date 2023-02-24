7fd8ae08eef1f92a49e9c64382b9755dcc685295
Japneet Sahni <japneet.sahni@arctiq.ca>
2023-02-24 06:31:42 -0500

    new set output method

--- .github/actions/hello/entrypoint.sh
+++ .github/actions/hello/entrypoint.sh
@@ -2,4 +2,8 @@
 
 echo "Hello $1"
 time=$(date)
-echo "::set-output name=time::$time"
+#echo "::set-output name=time::$time"
+echo "time=$time" >> $GITHUB_OUTPUT
+
+#echo '::set-env name=HELLO::hello'
+echo "HELLO=hello" >> $GITHUB_ENV
